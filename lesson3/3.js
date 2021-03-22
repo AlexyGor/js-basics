7e58bb6de2daacb5f1be8188a74f056e6bd841a9
AlexyGor <lextver@gmail.com>
2021-03-22 14:11:41 +0300

    add lesson3

---
+++ lesson3/3.js
@@ -0,0 +1,38 @@
+/*
+3 Перед вами находится массив с продуктами, сегодня распродажа и вам нужно на каждый товар применить скидку
+15%, можете использовать метод forEach https://mzl.la/1AOMMWX :
+const products = [
+{
+id: 3,
+price: 200,
+},
+{
+id: 4,
+price: 900,
+},
+{
+id: 1,
+price: 1000,
+},
+];
+ */
+
+"use strict";
+    const products = [
+        {
+            id: 3,
+            price: 200,
+        },
+        {
+            id: 4,
+            price: 900,
+        },
+        {
+            id: 1,
+            price: 1000,
+        },
+    ];
+    products.forEach(items => {
+        items.price = items.price - items.price * 0.15;
+    });
+    console.log(products);
